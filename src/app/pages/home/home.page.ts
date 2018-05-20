c6883da9f144b26b379047eae3a0e8310bf3651d
JaeHo Song <thruthesky@gmail.com>
2018-05-21 00:33:39 +0900

    user register, update, login, logout

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,26 +1,16 @@
 import { Component } from '@angular/core';
-import { PhilGoApiService, ApiCurrencyResponse } from '../../modules/philgo-api/providers/philgo-api.service';
-
+import { ApiCurrencyResponse } from '../../modules/philgo-api/providers/philgo-api.service';
+import { PhilGoApiTestService } from '../../modules/philgo-api/providers/philgo-api-test.service';
 @Component({
   selector: 'app-page-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  providers: [PhilGoApiTestService]
 })
 export class HomePage {
   constructor(
-    public api: PhilGoApiService
+    t: PhilGoApiTestService
   ) {
-    api.setUrl( 'https://local.philgo.com/api.php' );
-    api.version().subscribe( re => {
-      console.log('version: ', re.version);
-    }, e => console.log(`Error code: ${e.code}, message: ${e.message}`));
-    api.exchangeRate().subscribe( re => {
-      console.log('exchangeRate: ', re);
-    }, e => console.log(`Error code: ${e.code}, message: ${e.message}`));
-
-    api.query<ApiCurrencyResponse>('exchangeRate', {currency: 'php'}).subscribe( re => {
-      console.log('usd: ', re.usd);
-      console.log('php: ', re.php);
-    }, e => console.log(e));
+    t.setUrl('https://local.philgo.com/api.php').run();
   }
 }
