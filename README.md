76275ed9eae9707f212b0f27dba6cda06148e718
JaeHo Song <thruthesky@gmail.com>
2018-05-25 11:45:50 +0800

    post list

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # philgo-family
+
+## todos
+
+* forum. qna, freetalk.
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ApiCurrencyResponse, PhilGoApiService } from '../../modules/philgo-api/providers/philgo-api.service';
 import { PhilGoApiTestService } from '../../modules/philgo-api/providers/philgo-api-test.service';
+import { PostListComponent } from '../../modules/philgo-api/components/forum/post-list/post-list.component';
 @Component({
   selector: 'app-page-home',
   templateUrl: 'home.page.html',
@@ -8,6 +9,7 @@ import { PhilGoApiTestService } from '../../modules/philgo-api/providers/philgo-
   providers: [PhilGoApiTestService]
 })
 export class HomePage {
+  @ViewChild('postListComponent') postListComponent: PostListComponent = null;
   show = {
     page: ''
   };
@@ -17,6 +19,13 @@ export class HomePage {
   ) {
     // t.setUrl('https://local.philgo.com/api.php').run();
     // api.deleteFile(1).subscribe( res => console.log('res: ', res), e => console.log('ERROR: ', e));
+  }
+  onClickPostList( id ) {
+    // this.show.page = 'post-list';
+    // this.postId = 'freetalk';
+
+    console.log( this.postListComponent );
+    this.postListComponent.loadPage({ post_id: id });
   }
 }
 
